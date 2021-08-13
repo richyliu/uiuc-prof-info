320dcdfb5af7c45ee7db05567ba575bac3a83a60
Jason Yip <jasonyip1234567@gmail.com>
2021-08-12 19:19:18 -0700

    [ADD]: Python template, data_cleaner folder

---
+++ templates/template.py
@@ -0,0 +1,13 @@
+import sys
+import argparse
+
+
+def main(*args):
+    parser = argparse.ArgumentParser()
+
+    args = argparse.parse_args(args)
+
+
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
